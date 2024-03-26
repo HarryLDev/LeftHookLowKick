805835173d79956c8e539cfe420cbc9e94b117ba
Harry Liu <harry.s.liu@gmail.com>
2024-03-26 16:03:51 -0400

    made main menu

---
+++ src/app/difficulty-select/difficulty-select.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-difficulty-select',
+  standalone: true,
+  imports: [],
+  templateUrl: './difficulty-select.component.html',
+  styleUrl: './difficulty-select.component.css'
+})
+export class DifficultySelectComponent {
+
+}
