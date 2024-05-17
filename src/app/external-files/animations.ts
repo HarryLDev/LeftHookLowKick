ed886eddbdb13b74257bf3faa4ea37f6d3a06812
Harry Liu <harry.s.liu@gmail.com>
2024-05-17 15:50:31 -0400

    Auto-generated commit

--- src/app/external-files/animations.ts
+++
@@ -1,8 +0,0 @@
-import { trigger, transition, style, animate } from '@angular/animations';
-
-export const fadeInAnimation = trigger('fadeInAnimation', [
-  transition(':enter', [
-    style({ opacity: 0 }),
-    animate('500ms', style({ opacity: 1 }))
-  ])
-]);
